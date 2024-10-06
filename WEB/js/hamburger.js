a66c0da02ef52f99b592ad39f0206d5683bd9786
Roberts Grišins <rg00220@rvt.lv>
2024-10-07 00:57:44 +0300

    Izveidots Django projekts, uzlabota CSS stilizācija, reponsivitāte.

--- WEB/js/hamburger.js
+++ WEB/js/hamburger.js
@@ -1,17 +1,42 @@
-var hamburger = document.getElementById("hamburger");
-var content = document.querySelector('.hamburger .dropdown-content');
+var hamburger = document.querySelector(".hamburger");
+var hamburger_btn = document.getElementById("hamburger");
+var hamburger_content = document.querySelector('.hamburger .dropdown-content');
+var hamburger_dropdowns = document.querySelectorAll(".hamburger-dropdown");
 
-hamburger.onclick = function(){
-    if (content.style.display == "block"){
-        content.style.display = "none";
+hamburger_btn.onclick = function(){
+    if (hamburger_content.style.display == "block"){
+        closeDropdown();
     } else{
-        content.style.display = "block";
-        content.style.animation = "dropdown-slide 0.5s";
+        hamburger_content.style.display = "block";
+        hamburger_content.style.animation = "dropdown-slide 0.5s";
     }
 
     window.addEventListener('click', function(event) {
         if (!hamburger.contains(event.target)){
-            content.style.display = "none";
+            hamburger_content.style.display = "none";
         }
     });
+
+    window.addEventListener('resize', function() {
+        closeDropdown();
+    });
 };
+
+hamburger_dropdowns.forEach(hamburger_dropdown =>{
+    hamburger_dropdown.onclick = function(){
+        var hamburger_dropdown_content = hamburger_dropdown.querySelector(".hamburger-dropdown-content");
+        var hamburger_dropdown_title = hamburger_dropdown.querySelector("a");
+
+        if(hamburger_dropdown_content.style.display == "block"){
+            hamburger_dropdown_content.style.display = "none"
+            hamburger_dropdown_title.style.backgroundColor = "#504d4d"
+        } else{
+            hamburger_dropdown_content.style.display = "block";
+            hamburger_dropdown_title.style.backgroundColor = "#444";
+        }
+    }
+});
+
+function closeDropdown(){
+    hamburger_content.style.display = "none";
+}
--- WEB/js/modal.js
+++ WEB/js/modal.js
@@ -4,6 +4,15 @@ var btns = document.querySelectorAll(".card-btn");
 
 var span = document.getElementById("close");
 
+var dict = {
+    "descriptions" : {
+        "Python" : "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
+        "Java" : "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let programmers write once, run anywhere.",
+        "C#" : "C# is a general-purpose high-level programming language supporting multiple paradigms. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented (class-based), and component-oriented programming disciplines.",
+        "PHP" : "PHP is a general-purpose scripting language geared towards web development. PHP code is usually processed on a web server by a PHP interpreter implemented as a module."
+    }
+}
+
 btns.forEach(btn =>{
 
     var card = btn.closest(".card");
@@ -11,10 +20,12 @@ btns.forEach(btn =>{
     var title = card.querySelector(".card-title").textContent;
 
     var modalTitle = document.getElementById("modal-title");
+    var modalDesc = document.getElementById("modal-description");
 
 
     btn.onclick = function(){
         modalTitle.textContent = title;
+        modalDesc.textContent = dict["descriptions"][title];
         modal.style.display = "flex";
     }
 
